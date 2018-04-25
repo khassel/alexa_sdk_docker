f7219cbd60498d649d54b1797809ead4f446b852
Karsten Hassel <hassel@gmx.de>
2018-04-25 23:51:32 +0200

    set default run-params in Dockerfile, create AlexaClientSDKConfig.json in entrypoint.sh [ci skip]

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -9,16 +9,17 @@ else
   echo "Audio forced to HDMI."
 fi
 
-# gehört auf dem Host gesetzt, nicht hier
-#if [ "$SetVolumeToMax" == "true" ]; then
-#  # Lautstärke 400=Max
-#  amixer cset numid=1 400
-#fi
-#
-#if [ "$SetVolumeToMax" == "true" ]; then
-#  # Micro Empfindlichkeit 55=Max
-#  amixer -c 1 cset numid=3,iface=MIXER,name='Mic Capture Volume' 31,55
-#fi  
+cp /srv/sdk-folder/sdk-source/avs-device-sdk/Integration/AlexaClientSDKConfig.json .
+sed -i "s:\${SDK_CBL_AUTH_DELEGATE_DATABASE_FILE_PATH}:$SDK_CBL_AUTH_DELEGATE_DATABASE_FILE_PATH:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_CONFIG_DEVICE_SERIAL_NUMBER}:$SDK_CONFIG_DEVICE_SERIAL_NUMBER:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_CONFIG_CLIENT_ID}:$SDK_CONFIG_CLIENT_ID:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_CONFIG_PRODUCT_ID}:$SDK_CONFIG_PRODUCT_ID:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_MISC_DATABASE_FILE_PATH}:$SDK_MISC_DATABASE_FILE_PATH:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_SQLITE_DATABASE_FILE_PATH}:$SDK_SQLITE_DATABASE_FILE_PATH:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_SQLITE_SETTINGS_DATABASE_FILE_PATH}:$SDK_SQLITE_SETTINGS_DATABASE_FILE_PATH:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SETTING_LOCALE_VALUE}:$SETTING_LOCALE_VALUE:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_CERTIFIED_SENDER_DATABASE_FILE_PATH}:$SDK_CERTIFIED_SENDER_DATABASE_FILE_PATH:" ./AlexaClientSDKConfig.json
+sed -i "s:\${SDK_NOTIFICATIONS_DATABASE_FILE_PATH}:$SDK_NOTIFICATIONS_DATABASE_FILE_PATH:" ./AlexaClientSDKConfig.json
 
 cd ./sdk-build
 
@@ -31,5 +32,5 @@ fi
 
 cd ./SampleApp/src \
   && ./SampleApp \
-  /srv/sdk-folder/sdk-build/Integration/AlexaClientSDKConfig.json \
+  /srv/sdk-folder/AlexaClientSDKConfig.json \
   /srv/sdk-folder/third-party/snowboy/resources/alexa/alexa-avs-sample-app/ $1
